4ed51dc68fabc5efc10770fbd9d043e7cdba7858
github-action-benchmark <github@users.noreply.github.com>
2025-09-11 21:14:55 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 678d35f1842fe1b1f160d965fbffe1ae234ec2c4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757582081564,
+  "lastUpdate": 1757625295204,
   "repoUrl": "https://github.com/ehtick/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -1139,6 +1139,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.83%",
             "unit": "ops/sec",
             "extra": "234 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kshabelko@microsoft.com",
+            "name": "Konstantin Shabelko",
+            "username": "konstantin-msft"
+          },
+          "committer": {
+            "email": "kshabelko@microsoft.com",
+            "name": "Konstantin",
+            "username": "konstantin-msft"
+          },
+          "distinct": true,
+          "id": "678d35f1842fe1b1f160d965fbffe1ae234ec2c4",
+          "message": "- Fix formatting",
+          "timestamp": "2025-09-11T16:57:22-04:00",
+          "tree_id": "f5a8b3936ef4fe5e94db34e5665b2ecaf5f0588a",
+          "url": "https://github.com/ehtick/microsoft-authentication-library-for-js/commit/678d35f1842fe1b1f160d965fbffe1ae234ec2c4"
+        },
+        "date": 1757625293056,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 240365,
+            "range": "±0.91%",
+            "unit": "ops/sec",
+            "extra": "233 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 256818,
+            "range": "±0.71%",
+            "unit": "ops/sec",
+            "extra": "239 samples"
           }
         ]
       }
