7cd01ce717e76d76925ebc5549eda33d024056fc
github-action-benchmark <github@users.noreply.github.com>
2025-11-10 22:31:19 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 2f011c9d89a0b816d3c7c6d4e359945c0ebf6584

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762420591520,
+  "lastUpdate": 1762813879514,
   "repoUrl": "https://github.com/ehtick/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -1975,6 +1975,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.90%",
             "unit": "ops/sec",
             "extra": "234 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "87724641+Robbie-Microsoft@users.noreply.github.com",
+            "name": "Robbie-Microsoft",
+            "username": "Robbie-Microsoft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2f011c9d89a0b816d3c7c6d4e359945c0ebf6584",
+          "message": "msal-node: Added Logging to HttpClient (#8101)",
+          "timestamp": "2025-11-10T10:12:44-05:00",
+          "tree_id": "0a4e05143e2edd3769a96368fe730b54237ca95f",
+          "url": "https://github.com/ehtick/microsoft-authentication-library-for-js/commit/2f011c9d89a0b816d3c7c6d4e359945c0ebf6584"
+        },
+        "date": 1762813877806,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 240077,
+            "range": "±0.77%",
+            "unit": "ops/sec",
+            "extra": "235 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 241762,
+            "range": "±0.96%",
+            "unit": "ops/sec",
+            "extra": "225 samples"
           }
         ]
       }
